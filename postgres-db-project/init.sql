e746aac71eef1deaa17994207c5832c189ca3284
Bedagya-Joshi <bedagya123@gmail.com>
2025-06-18 14:06:22 +0545

    Docker

--- postgres-db-project/init.sql
+++ postgres-db-project/init.sql
@@ -1,25 +1,30 @@
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(10) CHECK (role IN ('admin', 'user')),
     failed_attempts INT DEFAULT 0,
     lockout_until TIMESTAMP
 );
 
-CREATE TABLE ItemCategory (
+INSERT INTO Users (username, password, role)
+SELECT 'admin', 'pbkdf2:sha256:600000$VoWEL4wgYV6GbJzk$0533b5b5455e6cddc9e032ebd5acffa000ea2a25e65ce680bf635cf536bd471f', 'admin'
+WHERE NOT EXISTS (SELECT 1 FROM Users WHERE username = 'admin');
+
+CREATE TABLE IF NOT EXISTS ItemCategory (
     category_id SERIAL PRIMARY KEY,
-    category_name VARCHAR(50) NOT NULL
+    category_name VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE Item (
+CREATE TABLE IF NOT EXISTS Item (
     item_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     quantity INT NOT NULL,
-    category_id INT REFERENCES ItemCategory(category_id)
+    category_id INT REFERENCES ItemCategory(category_id),
+    user_id INT REFERENCES Users(user_id)
 );
 
-CREATE TABLE ItemRequested (
+CREATE TABLE IF NOT EXISTS ItemRequested (
     id SERIAL PRIMARY KEY,
     date_of_request TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INT REFERENCES Users(user_id),
@@ -29,7 +34,7 @@ CREATE TABLE ItemRequested (
     status VARCHAR(20) DEFAULT 'Pending'
 );
 
-CREATE TABLE ItemHistory (
+CREATE TABLE IF NOT EXISTS ItemHistory (
     id SERIAL PRIMARY KEY,
     date_of_request TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_of_approve TIMESTAMP,
